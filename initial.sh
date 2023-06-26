12717b5590eda08f5eab28a9a1b381f2a8eb2239
stelee410 <129747297+stelee410@users.noreply.github.com>
2023-06-26 19:32:15 +0800

    fix minor issue of ubuntu bitsandbytes lib

--- initial.sh
+++ initial.sh
@@ -1,5 +1,6 @@
 python3 -m pip install --upgrade pip
 pip install -r https://raw.githubusercontent.com/stelee410/lambda-utility/main/requirements.txt
 pip install protobuf==3.20.*
+cp /home/ubuntu/.local/lib/python3.8/site-packages/bitsandbytes/libbitsandbytes_cuda117.so /home/ubuntu/.local/lib/python3.8/site-packages/bitsandbytes/libbitsandbytes_cpu.so
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
