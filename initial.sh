6ec94fdd3cdb193c141f55ee1170430f95d4fef4
stelee410 <129747297+stelee410@users.noreply.github.com>
2023-06-26 18:32:44 +0800

    install lower version of protobuf

--- initial.sh
+++ initial.sh
@@ -1,4 +1,5 @@
 python3 -m pip install --upgrade pip
 pip install -r https://raw.githubusercontent.com/stelee410/lambda-utility/main/requirements.txt
+pip install protobuf==3.20.*
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
